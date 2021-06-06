05b4a52a69b585ce2bf35a8b398604c47bcb6759
vlad-5262 <vladislavinkov@yandex.ru>
2021-06-06 21:42:41 +0300

    create: javascript slider

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".slider-button--prev",
+    prevEl: ".slider-button--next",
+  },
+  effect: "coverflow",
+});
